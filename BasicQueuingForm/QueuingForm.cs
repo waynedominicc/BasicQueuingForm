a96db61d0b68d0c80d22e4636c8301a4de135496
user <user@DESKTOP-URDDVLJ>
2023-09-16 22:12:50 +0800

    Add project files.

---
+++ BasicQueuingForm/QueuingForm.cs
@@ -0,0 +1,27 @@
+namespace BasicQueuingForm
+{
+    public partial class QueuingForm : Form
+
+    {
+        private CashierClass cashier = new CashierClass();
+        public QueuingForm()
+        {
+            InitializeComponent();
+            cashier = new CashierClass();
+            CashierWindowQueueForm obj = new CashierWindowQueueForm();
+            obj.Show();
+        }
+
+        private void btnCashier_Click(object sender, EventArgs e)
+        {
+            lblQueue.Text = cashier.CashierGeneratedNumber("P - ");
+            CashierClass.getNumberInQueue = lblQueue.Text;
+            CashierClass.CashierQueue.Enqueue(CashierClass.getNumberInQueue);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            CashierWindowQueueForm cashierWindow = new CashierWindowQueueForm();
+        }
+    }
+}
